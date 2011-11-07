01f9227683a382ee1041aa99cf12e642903d4aa6
LucaCavazzana <LucaCavazzana@cabb85ed-0cbc-ef9d-a035-de6b63d18cc2>
2011-11-07 16:38:38 +0000

    src characters fixed

--- trunk/src/Lisi/findBurstEMG.m
+++ trunk/src/Lisi/findBurstEMG.m
@@ -1,251 +1,250 @@
-1 %% FindBurstEMG
-2 % Function to find the edges of each burst
-3 %
-4 % By Giuseppe Lisi for Politecnico di Milano
-5 % beppelisi@gmail.com
-6 % 8 June 2010
-7 %% Inputs
-8 % signal1: is the liear envelope of the signal coming from
-9 % Channel 1.
-10 %
-11 % signal2: is the liear envelope of the signal coming from
-12 % Channel 2.
-13 %
-14 % signal3: is the liear envelope of the signal coming from
-15 % Channel 3.
-16 %
-17 % debug=1: to pause the segmentation phase and plot the figures
-18 % of each segemented signal. Debug mode
-19 %
-20 % ch2=1: if the second channel is used.
-21 %
-22 % ch3=1: if the third channel is used.
-23 %% Outputs
-24 % secondDivision: vector containing all the ending edges of the
-25 % bursts
-26 %xiv Appendix A. The Implementation of the Project
-27 % firstDivision: vector conaining all the starting edges of the
-28 % bursts
-29 %%
-30 function [firstDivision,secondDivision]=...
-31 findBurstEMG(signal1,signal2,signal3,debug,ch2,ch3)
-32
-33 ls=length(signal1); %length of the signal
-34 firstDivision=[];
-35 secondDivision=[];
-36
-37 %54 samples correspond to 0.2 seconds of signal(sampling rate
-38 % 270Samp/Sec) normal burst duration corresponding to 1second
-39 sampleDur=54*5;
-40
-41 %normal movement
-42 delay=40;
-43
-44 %short movement
-45 %delay=20;
-46
-47 %the lower level under which it is impossible to start a burst
-48 cost=10;
-49
-50 %factor for which the initial part of the moving average is
-51 %computed in order to avoid fake initial bursts
-52 mult=30;
-53
-54
-55 %once the burst has been detected its edges have to be shifted
-56 %back of this value
-57 back=100;
-58
-59 % contain the value of the next ending edge. Equal to 1 if the
-60 % start still have to be found
-61 next1=1;
-62 next2=1;
-63 next3=1;
-64
-65 %sum for the threshold computation.
-66 sum1=signal1(1)*mult;
-67 sum2=signal2(1)*mult;
-68 sum3=signal3(1)*mult;
-69
-70 %threshold for the three channels
-71 thr1(1)=sum1;
-72 thr2(1)=sum2;
-73 thr3(1)=sum3;
-74
-75 % records the highest value found so far in all the threeA.2. Arti?cial Neural Network Training xv
-76 % channels
-77 max=0;
-78
-79 %1 if first channel, 2 if second 3 if third
-80 choice=0;
-81
-82 %restart=1 if the system is ready to detect a new burst
-83 restart=0;
-84
-85 % empiric values for the decision to take about the burst
-86 % start.
-87 perc=22/100;
-88 clos=1/20;
-89
-90 % burst edges detection
-91 for i=2:ls
-92
-93 sum1=sum1+signal1(i);
-94 thr1(i)=sum1/i;
-95 sum2=sum2+signal2(i);
-96 thr2(i)=sum2/i;
-97 sum3=sum3+signal3(i);
-98 thr3(i)=sum3/i;
-99
-100 if(signal1(i)?thr1(i)+perc*thr1(i) &&...
-101 next1==1 && i>restart && signal1(i)>cost)
-102 % prev contains the starting point of the edge.
-103 prev1=i;
-104 if(prev1-back>1)
-105 prev1=prev1-back;
-106 if(prev1+sampleDur<ls)
-107 next1=prev1+sampleDur;
-108 else
-109 next1=1;
-110 end
-111 else
-112 prev1=1;
-113 next1=1+sampleDur;
-114 end
-115 end
-116
-117 if(i==next1)
-118 %if the signal is still high -> delay the closing
-119 %of the burst
-120 if(signal1(i)>thr1(i)-clos*thr1(i))
-121 if(next1+delay<ls)
-122 next1=next1+delay;
-123 else
-124 next1=ls;xvi Appendix A. The Implementation of the Project
-125 end
-126 else
-127 if(choice==1)
-128
-129 firstDivision=[firstDivision prev1];
-130 secondDivision=[secondDivision next1];
-131 max=0;
-132 choice1=0;
-133 restart=next1+back;
-134 next1=1;
-135 next2=1;
-136 next3=1;
-137 end
-138 end
-139 end
-140
-141 if(ch2)
-142 if(signal2(i)?thr2(i)+perc*thr2(i) && next2==1 &&...
-143 i>restart && signal2(i)>cost)
-144
-145 prev2=i;
-146 if(prev2-back>1)
-147 prev2=prev2-back;
-148
-149 if(prev2+sampleDur<ls)
-150 next2=prev2+sampleDur;
-151 else
-152 next2=1;
-153 end
-154 else
-155 prev2=1;
-156 next2=1+sampleDur;
-157 end
-158 end
-159 if(i==next2)
-160 %if the signal is still high delay -> the closing
-161 %of the burst
-162 if(signal2(i)>thr2(i)-clos*thr2(i))
-163 if(next2+delay<ls)
-164 next2=next2+delay;
-165 else
-166 next2=ls;
-167 end
-168 else
-169
-170 if(choice==2)
-171 firstDivision=[firstDivision prev2];
-172 secondDivision=[secondDivision next2];
-173 max=0;A.2. Arti?cial Neural Network Training xvii
-174 choice2=0;
-175 restart=next2+back;
-176 next1=1;
-177 next2=1;
-178 next3=1;
-179 end
-180 end
-181 end
-182 end
-183
-184 if(ch3)
-185 if(signal3(i)?thr3(i)+perc*thr3(i) && next3==1 &&...
-186 i>restart && signal3(i)>cost)
-187
-188 prev3=i;
-189
-190 if(prev3-back>1)
-191 prev3=prev3-back;
-192 if(prev3+sampleDur<ls)
-193 next3=prev3+sampleDur;
-194 else
-195 next3=1;
-196 end
-197 else
-198 prev3=1;
-199 next3=1+sampleDur;
-200 end
-201 end
-202
-203 if(i==next3)
-204 %if the signal is still high -> delay the
-205 %closing of the burst
-206 if(signal3(i)>thr3(i)-clos*thr3(i))
-207 if(next3+delay<ls)
-208 next3=next3+delay;
-209 else
-210 next3=ls;
-211 end
-212 else
-213
-214 if(choice==3)
-215 firstDivision=[firstDivision prev3];
-216 secondDivision=[secondDivision next3];
-217 max=0;
-218 choice3=0;
-219 restart=next3+back;
-220 next1=1;
-221 next2=1;
-222 next3=1;xviii Appendix A. The Implementation of the Project
-223 end
-224 end
-225 end
-226 end
-227
-228 if signal1(i)>max && signal1(i)?thr1(i)+perc*thr1(i)
-229 max=signal1(i);
-230 choice=1;
-231 end
-232 if signal2(i)>max && ch2 && signal2(i)?thr2(i)+...
-233 perc*thr2(i)
-234 max=signal2(i);
-235 choice=2;
-236 end
-237 if signal3(i)>max && ch3 && signal3(i)?thr3(i)+...
-238 perc*thr3(i)
-239 max=signal3(i);
-240 choice=3;
-241 end
-242 end
-243
-244
-245 % if there a burst start has been detected, but not the end,
-246 % eliminate the
-247 % burst
-248 if(¬isempty(firstDivision) && ...
-249 length(firstDivision)>length(secondDivision))
-250 firstDivision=firstDivision(1:length(secondDivision));
-251 end
+%% FindBurstEMG
+% Function to find the edges of each burst
+%
+% By Giuseppe Lisi for Politecnico di Milano
+% beppelisi@gmail.com
+% 8 June 2010
+%% Inputs
+% signal1: is the liear envelope of the signal coming from
+% Channel 1.
+%
+% signal2: is the liear envelope of the signal coming from
+% Channel 2.
+%
+% signal3: is the liear envelope of the signal coming from
+% Channel 3.
+%
+% debug=1: to pause the segmentation phase and plot the figures
+% of each segemented signal. Debug mode
+%
+% ch2=1: if the second channel is used.
+%
+% ch3=1: if the third channel is used.
+%% Outputs
+% secondDivision: vector containing all the ending edges of the
+% bursts
+% firstDivision: vector conaining all the starting edges of the
+% bursts
+%%
+function [firstDivision,secondDivision]=...
+    findBurstEMG(signal1,signal2,signal3,debug,ch2,ch3)
+
+ls=length(signal1); %length of the signal
+firstDivision=[];
+secondDivision=[];
+
+%54 samples correspond to 0.2 seconds of signal(sampling rate
+% 270Samp/Sec) normal burst duration corresponding to 1second
+sampleDur=54*5;
+
+%normal movement
+delay=40;
+
+%short movement
+%delay=20;
+
+%the lower level under which it is impossible to start a burst
+cost=10;
+
+%factor for which the initial part of the moving average is
+%computed in order to avoid fake initial bursts
+mult=30;
+
+
+%once the burst has been detected its edges have to be shifted
+%back of this value
+back=100;
+
+% contain the value of the next ending edge. Equal to 1 if the
+% start still have to be found
+next1=1;
+next2=1;
+next3=1;
+
+%sum for the threshold computation.
+sum1=signal1(1)*mult;
+sum2=signal2(1)*mult;
+sum3=signal3(1)*mult;
+
+%threshold for the three channels
+thr1(1)=sum1;
+thr2(1)=sum2;
+thr3(1)=sum3;
+
+% records the highest value found so far in all the threeA.2. Arti?cial Neural Network Training xv
+% channels
+max=0;
+
+%1 if first channel, 2 if second 3 if third
+choice=0;
+
+%restart=1 if the system is ready to detect a new burst
+restart=0;
+
+% empiric values for the decision to take about the burst
+% start.
+perc=22/100;
+clos=1/20;
+
+% burst edges detection
+for i=2:ls
+    
+    sum1=sum1+signal1(i);
+    thr1(i)=sum1/i;
+    sum2=sum2+signal2(i);
+    thr2(i)=sum2/i;
+    sum3=sum3+signal3(i);
+    thr3(i)=sum3/i;
+    
+    if(signal1(i)>=thr1(i)+perc*thr1(i) &&...
+            next1==1 && i>restart && signal1(i)>cost)
+        % prev contains the starting point of the edge.
+        prev1=i;
+        if(prev1-back>1)
+            prev1=prev1-back;
+            if(prev1+sampleDur<ls)
+                next1=prev1+sampleDur;
+            else
+                next1=1;
+            end
+        else
+            prev1=1;
+            next1=1+sampleDur;
+        end
+    end
+    
+    if(i==next1)
+        %if the signal is still high -> delay the closing
+        %of the burst
+        if(signal1(i)>thr1(i)-clos*thr1(i))
+            if(next1+delay<ls)
+                next1=next1+delay;
+            else
+                next1=ls;
+            end
+        else
+            if(choice==1)
+                
+                firstDivision=[firstDivision prev1];
+                secondDivision=[secondDivision next1];
+                max=0;
+                choice1=0;
+                restart=next1+back;
+                next1=1;
+                next2=1;
+                next3=1;
+            end
+        end
+    end
+    
+    if(ch2)
+        if(signal2(i)>=thr2(i)+perc*thr2(i) && next2==1 &&...
+                i>restart && signal2(i)>cost)
+            
+            prev2=i;
+            if(prev2-back>1)
+                prev2=prev2-back;
+                
+                if(prev2+sampleDur<ls)
+                    next2=prev2+sampleDur;
+                else
+                    next2=1;
+                end
+            else
+                prev2=1;
+                next2=1+sampleDur;
+            end
+        end
+        if(i==next2)
+            %if the signal is still high delay -> the closing
+            %of the burst
+            if(signal2(i)>thr2(i)-clos*thr2(i))
+                if(next2+delay<ls)
+                    next2=next2+delay;
+                else
+                    next2=ls;
+                end
+            else
+                
+                if(choice==2)
+                    firstDivision=[firstDivision prev2];
+                    secondDivision=[secondDivision next2];
+                    max=0;
+                    choice2=0;
+                    restart=next2+back;
+                    next1=1;
+                    next2=1;
+                    next3=1;
+                end
+            end
+        end
+    end
+    
+    if(ch3)
+        if(signal3(i)>=thr3(i)+perc*thr3(i) && next3==1 &&...
+                i>restart && signal3(i)>cost)
+            
+            prev3=i;
+            
+            if(prev3-back>1)
+                prev3=prev3-back;
+                if(prev3+sampleDur<ls)
+                    next3=prev3+sampleDur;
+                else
+                    next3=1;
+                end
+            else
+                prev3=1;
+                next3=1+sampleDur;
+            end
+        end
+        
+        if(i==next3)
+            %if the signal is still high -> delay the
+            %closing of the burst
+            if(signal3(i)>thr3(i)-clos*thr3(i))
+                if(next3+delay<ls)
+                    next3=next3+delay;
+                else
+                    next3=ls;
+                end
+            else
+                
+                if(choice==3)
+                    firstDivision=[firstDivision prev3];
+                    secondDivision=[secondDivision next3];
+                    max=0;
+                    choice3=0;
+                    restart=next3+back;
+                    next1=1;
+                    next2=1;
+                    next3=1;
+                end
+            end
+        end
+    end
+    
+    if signal1(i)>max && signal1(i)>=thr1(i)+perc*thr1(i)
+        max=signal1(i);
+        choice=1;
+    end
+    if signal2(i)>max && ch2 && signal2(i)>=thr2(i)+...
+            perc*thr2(i)
+        max=signal2(i);
+        choice=2;
+    end
+    if signal3(i)>max && ch3 && signal3(i)>=thr3(i)+...
+            perc*thr3(i)
+        max=signal3(i);
+        choice=3;
+    end
+end
+
+
+% if there a burst start has been detected, but not the end,
+% eliminate the
+% burst
+if(~isempty(firstDivision) && ...
+        length(firstDivision)>length(secondDivision))
+    firstDivision=firstDivision(1:length(secondDivision));
+end
--- trunk/src/Lisi/splitFilter.m
+++ trunk/src/Lisi/splitFilter.m
@@ -1,267 +1,267 @@
-1 %% Split Filter
-2 % This script is used to split the incoming signal and to
-3 % filter it.
-4 %
-5 % By Giuseppe Lisi for Politecnico di Milanoviii Appendix A. The Implementation of the Project
-6 % beppelisi@gmail.com
-7 % 8 June 2010
-8 %% Inputs
-9 % c: is the cell array containing all the signals in matlab
-10 % format.
-11 %
-12 % debug=1: to pause the segmentation phase and plot the figures
-13 % of each segemented signal. Debug mode
-14 %
-15 % acq=1: if the script is used during the acquisition phase
-16 %
-17 % np: (name of the person) is the name of the folder in which
-18 % are contained the training data.
-19 %
-20 % i: is the index representing the current single signal to
-21 % process.
-22 %
-23 % plotting=1: to save the figures of the segmented signals
-24 % inside the 'img'folder contained inside the np folder.
-25 % 'img' is automatically created.
-26 %
-27 % ch2=1: if the second channel is used.
-28 %
-29 % ch3=1: if the third channel is used.
-30 %% Outputs
-31 % f: is the cell array containing all the feature vector
-32 % related to the signal contained in c at the position i.
-33 %%
-34 function f=splitFilter(c,debug,acq,plotting,i,np,ch2,ch3)
-35
-36 c1=c{i,1};
-37 c2=c{i,2};
-38 c3=c{i,3};
-39 nsamp=c{i,4};
-40
-41 % Rectification
-42 y1=abs(c1-512);
-43 y2=abs(c2-512);
-44 y3=abs(c3-512);
-45
-46 f=[];
-47 f1=[];
-48 f2=[];
-49 f3=[];
-50
-51 %Linear envelope
-52 if(length(y1)6= 1)
-53
-54 freqCamp=270; %sampling frequencyA.2. Arti?cial Neural Network Training ix
-55 cutOffFreq=2; %cutoff frequency of the low-pass filter
-56 nyquistFreq=cutOffFreq/(freqCamp/2);
-57 [b,a]=butter(2,nyquistFreq);
-58 %filt is the envelope of the rectified signal
-59 filt1=filter(b,a,y1);
-60 filt1=filt1(50:length(filt1));
-61
-62
-63 filt2=filter(b,a,y2);
-64 filt2=filt2(50:length(filt2));
-65
-66
-67 filt3=filter(b,a,y3);
-68 filt3=filt3(50:length(filt3));
-69
-70
-71
-72 % find the edges of each burst
-73 [firstDiv,secondDiv]...
-74 =findBurstEMG(filt1,filt2,filt3,debug,ch2,ch3);
-75
-76
-77 %Filtering above 10 Hz
-78 cutoffF1=10;
-79 nyquistF=cutoffF1/(freqCamp/2);
-80 [num,den] = butter(2,nyquistF,'high');
-81 filtS=filter(num,den,c1);
-82 filtSign=filtS(50:length(filtS));
-83
-84 filtS2=filter(num,den,c2);
-85 filtSign2=filtS2(50:length(filtS2));
-86
-87 filtS3=filter(num,den,c3);
-88 filtSign3=filtS3(50:length(filtS3));
-89
-90 % the feature extraction is not performed during the
-91 % acquisition phase.
-92 if(¬acq)
-93
-94 for j=1:length(firstDiv)
-95 f1(j,:)=...
-96 extractFeatures(filtSign(firstDiv(j):secondDiv(j)));
-97 end
-98
-99
-100 if ch2
-101 for j=1:length(firstDiv)
-102 f2(j,:)=...
-103 extractFeatures(filtSign2(firstDiv(j):secondDiv(j)));x Appendix A. The Implementation of the Project
-104 end
-105 end
-106
-107 if ch3
-108 for j=1:length(firstDiv)
-109 f3(j,:)=...
-110 extractFeatures(filtSign3(firstDiv(j):secondDiv(j)));
-111 end
-112 end
-113
-114 if(¬isempty(firstDiv))
-115
-116 f=[f1 f2 f3];
-117 end
-118
-119 end
-120
-121 sum1=filt1(1)*100;
-122 sum2=filt2(1)*100;
-123 sum3=filt3(1)*100;
-124 thr1(1)=sum1;
-125 thr2(1)=sum2;
-126 thr3(1)=sum3;
-127 % computing the 'splitting threshold' in order to plot it
-128 for i=2:length(filt1)
-129 sum1=sum1+filt1(i);
-130 thr1(i)=sum1/i;
-131 sum2=sum2+filt2(i);
-132 thr2(i)=sum2/i;
-133 sum3=sum3+filt3(i);
-134 thr3(i)=sum3/i;
-135 end
-136
-137
-138
-139 if debug
-140 % Plots the segmentation of the envelope of the first
-141 % channel.
-142 figure;
-143 plot(1:length(filt1),filt1)
-144 hold on;
-145 plot(1:length(thr1),thr1,'y');
-146 axis([1 length(filt1) 0 150]);
-147 if(¬isempty(firstDiv))
-148 vline(firstDiv,'g','');
-149 vline(secondDiv,'r','');
-150
-151 end
-152 % Plots the segmentation of the envelope of the secondA.2. Arti?cial Neural Network Training xi
-153 % channel.
-154 figure;
-155 plot(1:length(filt2),filt2)
-156 hold on;
-157 plot(1:length(thr2),thr2,'y');
-158 axis([0 length(filt2) 0 150]);
-159 if(¬isempty(firstDiv))
-160 vline(firstDiv,'g','');
-161 vline(secondDiv,'r','');
-162
-163 end
-164
-165 % Plots the segmentation of the envelope of the third
-166 % channel.
-167 figure;
-168 plot(1:length(filt3),filt3)
-169 hold on;
-170 plot(1:length(thr3),thr3,'y');
-171 axis([0 length(filt3) 0 150]);
-172 if(¬isempty(firstDiv))
-173 vline(firstDiv,'g','');
-174 vline(secondDiv,'r','');
-175 end
-176
-177 % Plots the segmented and high-pass filtered signal of
-178 % Channel 1.
-179 figure;
-180 plot(1:length(filtSign),filtSign);
-181 axis([1 length(filtSign) -400 400]);
-182 if(¬isempty(firstDiv))
-183 vline(firstDiv,'g','');
-184 vline(secondDiv,'r','');
-185 end
-186
-187 % Plots the segmented and high-pass filtered signal of
-188 % Channel 2.
-189 if ch2
-190 figure;
-191 plot(1:length(filtSign2),filtSign2);
-192 axis([0 length(filtSign2) -400 400]);
-193 if(¬isempty(firstDiv))
-194 vline(firstDiv,'g','');
-195 vline(secondDiv,'r','');
-196 end
-197 end
-198
-199 % Plots the segmented and high-pass filtered signal of
-200 % Channel 3.
-201 if ch3xii Appendix A. The Implementation of the Project
-202 figure;
-203 plot(1:length(filtSign3),filtSign3);
-204 axis([0 length(filtSign3) -400 400]);
-205 if(¬isempty(firstDiv))
-206 vline(firstDiv,'g','');
-207 vline(secondDiv,'r','');
-208 end
-209 end
-210 numberOFMovements=length(firstDiv)
-211 if(¬acq)
-212 ginput(1);
-213 close all;
-214 end
-215
-216
-217 end
-218
-219 % saving the figures of the fitered and segmented signal
-220 % into the 'img' folder
-221 if plotting
-222 file2save=['/Users/giuseppelisi/University/Thesis/'...
-223 'Matlab/FilesNewEmg/serial/' np '/ch1/img/image'...
-224 sprintf('%d',nsamp) ' ' sprintf('%d',i) '.eps'];
-225 fig = figure('visible','off');
-226 plot(1:length(filtSign),filtSign,'b');
-227 axis([0 length(filtSign) -400 400]);
-228 if(¬isempty(firstDiv))
-229 vline(firstDiv,'g','');
-230 vline(secondDiv,'r','');
-231 end
-232 saveas(fig,file2save,'eps');
-233
-234 if ch2
-235 file2save=['/Users/giuseppelisi/University/Thesis/'...
-236 'Matlab/FilesNewEmg/serial/' np '/ch2/img/image'...
-237 sprintf('%d',nsamp) ' ' sprintf('%d',i) '.eps'];
-238 fig = figure('visible','off');
-239 plot(1:length(filtSign2),filtSign2,'b');
-240 axis([0 length(filtSign2) -400 400]);
-241 if(¬isempty(firstDiv))
-242 vline(firstDiv,'g','');
-243 vline(secondDiv,'r','');
-244 end
-245 saveas(fig,file2save,'eps');
-246 end
-247
-248
-249 if ch3
-250 file2save=['/Users/giuseppelisi/University/Thesis/'...A.2. Arti?cial Neural Network Training xiii
-251 'Matlab/FilesNewEmg/serial/' np '/ch3/img/image'...
-252 sprintf('%d',nsamp) ' ' sprintf('%d',i) '.eps'];
-253 fig = figure('visible','off');
-254 plot(1:length(filtSign3),filtSign3);
-255 axis([0 length(filtSign3) -400 400]);
-256 if(¬isempty(firstDiv))
-257 vline(firstDiv,'g','');
-258 vline(secondDiv,'r','');
-259 end
-260 saveas(fig,file2save,'eps');
-261 end
-262 end
-263
-264
-265
-266 end
-267 end
+%% Split Filter
+% This script is used to split the incoming signal and to
+% filter it.
+%
+% By Giuseppe Lisi for Politecnico di Milanoviii Appendix A. The Implementation of the Project
+% beppelisi@gmail.com
+% 8 June 2010
+%% Inputs
+% c: is the cell array containing all the signals in matlab
+% format.
+%
+% debug=1: to pause the segmentation phase and plot the figures
+% of each segemented signal. Debug mode
+%
+% acq=1: if the script is used during the acquisition phase
+%
+% np: (name of the person) is the name of the folder in which
+% are contained the training data.
+%
+% i: is the index representing the current single signal to
+% process.
+%
+% plotting=1: to save the figures of the segmented signals
+% inside the 'img'folder contained inside the np folder.
+% 'img' is automatically created.
+%
+% ch2=1: if the second channel is used.
+%
+% ch3=1: if the third channel is used.
+%% Outputs
+% f: is the cell array containing all the feature vector
+% related to the signal contained in c at the position i.
+%%
+function f=splitFilter(c,debug,acq,plotting,i,np,ch2,ch3)
+
+c1=c{i,1};
+c2=c{i,2};
+c3=c{i,3};
+nsamp=c{i,4};
+
+% Rectification
+y1=abs(c1-512);
+y2=abs(c2-512);
+y3=abs(c3-512);
+
+f=[];
+f1=[];
+f2=[];
+f3=[];
+
+%Linear envelope
+if(length(y1)~= 1)
+    
+    freqCamp=270; %sampling frequencyA.2. Arti?cial Neural Network Training ix
+    cutOffFreq=2; %cutoff frequency of the low-pass filter
+    nyquistFreq=cutOffFreq/(freqCamp/2);
+    [b,a]=butter(2,nyquistFreq);
+    %filt is the envelope of the rectified signal
+    filt1=filter(b,a,y1);
+    filt1=filt1(50:length(filt1));
+    
+    
+    filt2=filter(b,a,y2);
+    filt2=filt2(50:length(filt2));
+    
+    
+    filt3=filter(b,a,y3);
+    filt3=filt3(50:length(filt3));
+    
+    
+    
+    % find the edges of each burst
+    [firstDiv,secondDiv]...
+        =findBurstEMG(filt1,filt2,filt3,debug,ch2,ch3);
+    
+    
+    %Filtering above 10 Hz
+    cutoffF1=10;
+    nyquistF=cutoffF1/(freqCamp/2);
+    [num,den] = butter(2,nyquistF,'high');
+    filtS=filter(num,den,c1);
+    filtSign=filtS(50:length(filtS));
+    
+    filtS2=filter(num,den,c2);
+    filtSign2=filtS2(50:length(filtS2));
+    
+    filtS3=filter(num,den,c3);
+    filtSign3=filtS3(50:length(filtS3));
+    
+    % the feature extraction is not performed during the
+    % acquisition phase.
+    if(~acq)
+        
+        for j=1:length(firstDiv)
+            f1(j,:)=...
+                extractFeatures(filtSign(firstDiv(j):secondDiv(j)));
+        end
+        
+        
+        if ch2
+            for j=1:length(firstDiv)
+                f2(j,:)=...
+                    extractFeatures(filtSign2(firstDiv(j):secondDiv(j)));x Appendix A. The Implementation of the Project
+            end
+        end
+        
+        if ch3
+            for j=1:length(firstDiv)
+                f3(j,:)=...
+                    extractFeatures(filtSign3(firstDiv(j):secondDiv(j)));
+            end
+        end
+        
+        if(~isempty(firstDiv))
+            
+            f=[f1 f2 f3];
+        end
+        
+    end
+    
+    sum1=filt1(1)*100;
+    sum2=filt2(1)*100;
+    sum3=filt3(1)*100;
+    thr1(1)=sum1;
+    thr2(1)=sum2;
+    thr3(1)=sum3;
+    % computing the 'splitting threshold' in order to plot it
+    for i=2:length(filt1)
+        sum1=sum1+filt1(i);
+        thr1(i)=sum1/i;
+        sum2=sum2+filt2(i);
+        thr2(i)=sum2/i;
+        sum3=sum3+filt3(i);
+        thr3(i)=sum3/i;
+    end
+    
+    
+    
+    if debug
+        % Plots the segmentation of the envelope of the first
+        % channel.
+        figure;
+        plot(1:length(filt1),filt1)
+        hold on;
+        plot(1:length(thr1),thr1,'y');
+        axis([1 length(filt1) 0 150]);
+        if(~isempty(firstDiv))
+            vline(firstDiv,'g','');
+            vline(secondDiv,'r','');
+            
+        end
+        % Plots the segmentation of the envelope of the secondA.2. Arti?cial Neural Network Training xi
+        % channel.
+        figure;
+        plot(1:length(filt2),filt2)
+        hold on;
+        plot(1:length(thr2),thr2,'y');
+        axis([0 length(filt2) 0 150]);
+        if(~isempty(firstDiv))
+            vline(firstDiv,'g','');
+            vline(secondDiv,'r','');
+            
+        end
+        
+        % Plots the segmentation of the envelope of the third
+        % channel.
+        figure;
+        plot(1:length(filt3),filt3)
+        hold on;
+        plot(1:length(thr3),thr3,'y');
+        axis([0 length(filt3) 0 150]);
+        if(~isempty(firstDiv))
+            vline(firstDiv,'g','');
+            vline(secondDiv,'r','');
+        end
+        
+        % Plots the segmented and high-pass filtered signal of
+        % Channel 1.
+        figure;
+        plot(1:length(filtSign),filtSign);
+        axis([1 length(filtSign) -400 400]);
+        if(~isempty(firstDiv))
+            vline(firstDiv,'g','');
+            vline(secondDiv,'r','');
+        end
+        
+        % Plots the segmented and high-pass filtered signal of
+        % Channel 2.
+        if ch2
+            figure;
+            plot(1:length(filtSign2),filtSign2);
+            axis([0 length(filtSign2) -400 400]);
+            if(~isempty(firstDiv))
+                vline(firstDiv,'g','');
+                vline(secondDiv,'r','');
+            end
+        end
+        
+        % Plots the segmented and high-pass filtered signal of
+        % Channel 3.
+        if ch3xii Appendix A. The Implementation of the Project
+            figure;
+            plot(1:length(filtSign3),filtSign3);
+            axis([0 length(filtSign3) -400 400]);
+            if(~isempty(firstDiv))
+                vline(firstDiv,'g','');
+                vline(secondDiv,'r','');
+            end
+        end
+        numberOFMovements=length(firstDiv)
+        if(~acq)
+            ginput(1);
+            close all;
+        end
+        
+        
+    end
+    
+    % saving the figures of the fitered and segmented signal
+    % into the 'img' folder
+    if plotting
+        file2save=['/Users/giuseppelisi/University/Thesis/'...
+            'Matlab/FilesNewEmg/serial/' np '/ch1/img/image'...
+            sprintf('%d',nsamp) ' ' sprintf('%d',i) '.eps'];
+        fig = figure('visible','off');
+        plot(1:length(filtSign),filtSign,'b');
+        axis([0 length(filtSign) -400 400]);
+        if(~isempty(firstDiv))
+            vline(firstDiv,'g','');
+            vline(secondDiv,'r','');
+        end
+        saveas(fig,file2save,'eps');
+        
+        if ch2
+            file2save=['/Users/giuseppelisi/University/Thesis/'...
+                'Matlab/FilesNewEmg/serial/' np '/ch2/img/image'...
+                sprintf('%d',nsamp) ' ' sprintf('%d',i) '.eps'];
+            fig = figure('visible','off');
+            plot(1:length(filtSign2),filtSign2,'b');
+            axis([0 length(filtSign2) -400 400]);
+            if(~isempty(firstDiv))
+                vline(firstDiv,'g','');
+                vline(secondDiv,'r','');
+            end
+            saveas(fig,file2save,'eps');
+        end
+        
+        
+        if ch3
+            file2save=['/Users/giuseppelisi/University/Thesis/'...A.2. Arti?cial Neural Network Training xiii
+                'Matlab/FilesNewEmg/serial/' np '/ch3/img/image'...
+                sprintf('%d',nsamp) ' ' sprintf('%d',i) '.eps'];
+            fig = figure('visible','off');
+            plot(1:length(filtSign3),filtSign3);
+            axis([0 length(filtSign3) -400 400]);
+            if(~isempty(firstDiv))
+                vline(firstDiv,'g','');
+                vline(secondDiv,'r','');
+            end
+            saveas(fig,file2save,'eps');
+        end
+    end
+    
+    
+    
+end
+end
