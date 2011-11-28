6cdbc6049e55df6c02cf2984ef97d208c2cd83cb
LucaCavazzana <LucaCavazzana@cabb85ed-0cbc-ef9d-a035-de6b63d18cc2>
2011-11-28 18:59:19 +0000

    now Matlab serial acquisition looks promising

--- trunk/src/onlineRecogn.m
+++ trunk/src/onlineRecogn.m
@@ -1,16 +1,52 @@
 function onlineRecogn()
+%ONLINERECOGNITION  TODO
+%
+
+%  By Luca Cavazzana for Politecnico di Milano
+%  luca.cavazzana@gmail.com
+%  FIXME: update
 
 global PORT;
 global SERIALCOMM;
 global DEBUG;
 
-system([SERIALCOMM ' -d ' PORT ' &']);
+% system([SERIALCOMM ' -d ' PORT ' &']);
+% ch1 = fopen('ch1','r');
+% while(1)
+%     asd = fscanf(ch1, '%d', [1, Inf]);
+%     disp(asd);
+% end
 
-ch1 = fopen('ch1','r');
-
-while(1)
-    asd = fscanf(ch1, '%d', [1, Inf]);
-    disp(asd);
+try
+    fclose(instrfind({'Port','Status'},{PORT, 'open'}));
+catch e
 end
+
+% open & init
+board = serial(PORT, ...
+    'BaudRate', 57600, ...
+    'InputBufferSize',  4590); % >1 sec of data
+fopen(board);
+
+rem = [];
+
+for i = 1:10
+    try
+        while(board.BytesAvailable < 32)
+            pause(.0004);
+        end
+        out{i} = fscanf(board, '%c', board.BytesAvailable);
+        [ch{i} rem] = parseEMG(out{i}, rem);
+        
+        % TODO: analysis
+        
+    catch e
+        disp(e);
+    end
+end
+
+keyboard
+
+fclose(board);
 
 end
---
+++ trunk/src/parseEMG.m
@@ -0,0 +1,44 @@
+function [ch, rem] = parseEMG(data, rem)
+%PARSEEMG   return the parsed raw EMG data
+%   [CH REM] = PARSEEMG(DATA, REM) parses the raw ascii DATA acquired from
+%   the emg board (as [D:## ## ##]+) and returns it as a Nx3 matrix CH (a
+%   column for each channel). The returned REM vector is a string
+%   containing the chars of the last, incomplete set. The same chunk has to
+%   be feed as input for the next call of the function if you want multiple
+%   continuous acquisitions.
+%
+%   Beware! Too much time between two serial reading (ie: heavy
+%   computation) could cause to miss some emg sets, crashing this function
+%   (due to a mismatch between REM and the starting chunk of the new
+%   acquisition).
+
+%	By Luca Cavazzana for Politecnico di Milano
+%	luca.cavazzana@gmail.com
+%	FIXME: update
+
+ds = find(data == 'D');
+nSets = 0;
+
+if(exist('rem', 'var') && ~isempty(rem))
+    if(isempty(ds))
+        ch = [];
+        rem = [rem, data];
+        return;
+    else
+        ch(size(ds,2),3) = 0;    % preallocate
+        rem = [rem, data(1:ds(1))];
+        ch(1,:) = sscanf(rem(3:end),'%d')';
+        nSets = 1;
+    end
+end
+
+if(length(ds)>1)
+    for i = 2:length(ds)
+        nSets = nSets+1;
+        ch(nSets,:) = sscanf(data(ds(i-1)+2:ds(i)), '%d')';
+    end
+end
+
+rem = data(ds(end):end);
+
+end
